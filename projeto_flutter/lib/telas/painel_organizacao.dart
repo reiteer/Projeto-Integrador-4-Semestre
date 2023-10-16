e87f60a11fc0ab90e9c9cabf1a5f641601115ca1
LUCAS BARBAROTO MIOTTO <22.01388-0@maua.br>
2023-10-16 15:20:09 -0300

    Painel ADM front

--- projeto_flutter/lib/telas/painel_organizacao.dart
+++ projeto_flutter/lib/telas/painel_organizacao.dart
@@ -4,15 +4,84 @@ void main() {
   runApp(VisualizarOrganizacao());
 }
 
+class Organizacao {
+  final String nome;
+
+  Organizacao(this.nome);
+}
+
 class VisualizarOrganizacao extends StatelessWidget {
+  final List<Organizacao> Organizacaos = [
+    Organizacao('Setor A'),
+    Organizacao('RH'),
+    Organizacao('Seter B'),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Visualizar Organização"),
-      ),
-      body: Center(
-        child: Text("Conteúdo da página Visualizar Organização"),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Visualizar Organizações"),
+          centerTitle: true,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  // Ação a ser executada ao pressionar o botão de adicionar usuário
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.green,
+                ),
+                icon: Icon(Icons.add),
+                label: Text("Adicionar Nova Organização"),
+              ),
+              SizedBox(height: 16),  // Adiciona um espaço entre os botões e a DataTable
+              Center(
+                child: Container(
+                  width: 1000,
+                  child: DataTable(
+                    columns: [
+                      DataColumn(label: Text('Nome')),
+                      DataColumn(label: Text('Editar')),
+                      DataColumn(label: Text('Excluir')),
+                    ],
+                    rows: Organizacaos
+                        .map(
+                          (Organizacao) => DataRow(
+                            cells: [
+                              DataCell(Text(Organizacao.nome)),
+                              DataCell(IconButton(
+                                icon: Icon(Icons.edit, color: Colors.orange),
+                                onPressed: () {
+                                  // Ação a ser executada ao pressionar o ícone de edição
+                                },
+                              )),
+                              DataCell(IconButton(
+                                icon: Icon(Icons.delete, color: Colors.red),
+                                onPressed: () {
+                                  // Ação a ser executada ao pressionar o ícone de exclusão
+                                },
+                              )),
+                            ],
+                          ),
+                        )
+                        .toList(),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- projeto_flutter/lib/telas/painel_projetos.dart
+++ projeto_flutter/lib/telas/painel_projetos.dart
@@ -4,15 +4,84 @@ void main() {
   runApp(VisualizarProjetos());
 }
 
+class projeto {
+  final String nome;
+
+  projeto(this.nome);
+}
+
 class VisualizarProjetos extends StatelessWidget {
+  final List<projeto> usuarios = [
+    projeto('Projeto 1'),
+    projeto('Projeto 2'),
+    projeto('Projeto 3'),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Visualizar Projetos"),
-      ),
-      body: Center(
-        child: Text("Conteúdo da página Visualizar Projetos"),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Visualizar Projetos"),
+          centerTitle: true,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  // Ação a ser executada ao pressionar o botão de adicionar usuário
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.green,
+                ),
+                icon: Icon(Icons.add),
+                label: Text("Adicionar Novo Projeto"),
+              ),
+              SizedBox(height: 16),  // Adiciona um espaço entre os botões e a DataTable
+              Center(
+                child: Container(
+                  width: 1000,
+                  child: DataTable(
+                    columns: [
+                      DataColumn(label: Text('Nome')),
+                      DataColumn(label: Text('Editar')),
+                      DataColumn(label: Text('Excluir')),
+                    ],
+                    rows: usuarios
+                        .map(
+                          (usuario) => DataRow(
+                            cells: [
+                              DataCell(Text(usuario.nome)),
+                              DataCell(IconButton(
+                                icon: Icon(Icons.edit, color: Colors.orange),
+                                onPressed: () {
+                                  // Ação a ser executada ao pressionar o ícone de edição
+                                },
+                              )),
+                              DataCell(IconButton(
+                                icon: Icon(Icons.delete, color: Colors.red),
+                                onPressed: () {
+                                  // Ação a ser executada ao pressionar o ícone de exclusão
+                                },
+                              )),
+                            ],
+                          ),
+                        )
+                        .toList(),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
