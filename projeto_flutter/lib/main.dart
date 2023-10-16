5480ccf86be706c1cafd492b6e5cdd0c0b0e658c
LUCAS BARBAROTO MIOTTO <22.01388-0@maua.br>
2023-10-16 09:17:03 -0300

    Começo do Projeto

---
+++ projeto_flutter/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:app_flutter/telas/login_page.dart';
+import 'package:app_flutter/telas/organizacao_page.dart';
+import 'package:app_flutter/telas/perfil_page.dart';
+
+void main() {
+  runApp(MinhaApp());
+}
+
+class MinhaApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => PaginaDeLogin(),
+        '/organizacao': (context) => OrganizacaoPage(),
+        '/perfil': (context) => PaginaPerfil(),
+      },
+    );
+  }
+}
